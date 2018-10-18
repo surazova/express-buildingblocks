b3e042bed02b0e7a0d8866f82b06383334552a82
Sitora Urazova <sitoraurazova@gmail.com>
2018-10-18 17:50:05 +0000

    Set additional dynamic route and param to avoid duplication

--- app.js
+++ app.js
@@ -1,18 +1,54 @@
 var express = require('express');
 var app = express();
 
+var blocks = {
+    'Fixed': 'Fastened securely in position', 
+    'Movable': 'Capable of being moved',
+    'Rotating': 'Moving in a circle around its center'
+};
 
-// //not using middleware
-// app.get('/', function(request, response) {
-//     response.sendFile(__dirname + '/public/index.html');
-// });
+//Adding dynamic route 
+app.get('/blocks/:name', function(request, response) {
+    //var description = blocks[request.params.name]; //reading the request parameter and looking up the object's properties 
+    var name = request.params.name;
+    var block = name [0].toUpperCase() + name.slice(1).toLowerCase();
+    var description = blocks[block]; //block name is now in the same format as the properties in the blocks object
+    } else { 
+    response.json(description);
+}); 
+
+
+//another dynamic route 
+
+app.get)'/locations/:name', function(request, response) {
+    var name = request.params.name; 
+    var block = name[0].toUpperCase() + name.slice(1).toLowerCase();
+    
+});
+
+//to avoid duplication, you pass params 
+
+app.param('name', function(request, response, next) {
+    var name = request.params.name;
+    var block = name[0].toUpperCase() + name.slice(1).toLowerCase();
+    
+    requset.blockName = block; 
+    
+    next();
+});
+
 
 //using static middleware 
-app.use(express.static('public'));
+app.use(express.static('public'));   //to run: curl -i http://localhost:8080/blocks/Fixed
 
 app.get('/blocks', function(request, response) {  
     var blocks = ['Fixed', 'Movable', 'Rotating']; 
-    response.json(blocks);
+    if(request.query.limit >= 0) {
+        response.json(blocks.slice(0, request.query.limit));  //0, where the extraction begins and slice it all the way up, not including the number assigned to limit 
+        
+    } else {
+    response.json(blocks);  // use curl http://localhost:8080/blocks?limit=1
+    }
 });
 
 
