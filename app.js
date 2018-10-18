57e1aa96361627b660706606d6fc46647d56081c
Sitora Urazova <sitoraurazova@gmail.com>
2018-10-18 03:08:50 +0000

    Created public folder and contents, added middleware

--- app.js
+++ app.js
@@ -1,38 +1,24 @@
-var express = require ('express');
-var app = express();  //requiring the express library 
-
-//app.get function creates a route that accepts GET requests 
-
-app.get('/', function(request, response) {
-    response.send('Hello World');
-});
+var express = require('express');
+var app = express();
 
 
-app.get('/blocks', function(request, response) {  //this will change the response to JSON format 
-    var blocks = ['Fixed', 'Movable', 'Rotating']; //to run: curl -i http://localhost:8080/blocks 
+// //not using middleware
+// app.get('/', function(request, response) {
+//     response.sendFile(__dirname + '/public/index.html');
+// });
+
+//using static middleware 
+app.use(express.static('public'));
+
+app.get('/blocks', function(request, response) {  
+    var blocks = ['Fixed', 'Movable', 'Rotating']; 
     response.json(blocks);
 });
 
-app.get('/blocks', function(request, response) {
-    var blocks = '<ul><li>Fixed</li><li>Movable</li></ul>';
-    response.send(blocks);
-});
-
-app.get('/blocks', function(request, response) {
-    response.redirect(301, '/parts');
-});
 
 app.listen(process.env.PORT, process.env.IP, 8080, function(){
     console.log("You are listening on port 8080");
     
 });
 
-
-//another method: 
-// app.get('/', function(request, response) {
-//     response.write('Hello World');
-//     response.end();
-// });
-
-// app.listen(3000);
-
+app.listen(8080);
