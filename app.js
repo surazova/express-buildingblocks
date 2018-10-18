7afa53d3d17f375e375b578ee99b7db430afc5af
Sitora Urazova <sitoraurazova@gmail.com>
2018-10-18 03:23:36 +0000

    Setting parameters for reading url and response status using JSON

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 var express = require('express');
 var app = express();
 
+var blocks = {
+    'Fixed': 'Fastened securely in position', 
+    'Movable': 'Capable of being moved',
+    'Rotating': 'Moving in a circle around its center'
+};
 
-// //not using middleware
-// app.get('/', function(request, response) {
-//     response.sendFile(__dirname + '/public/index.html');
-// });
+//Adding dynamic route 
+app.get('/blocks/:name', function(request, response) {
+    var description = blocks[request.params.name];
+    if (!description) { //flips to boolean
+        response.status(404).json('No description found for ' + request.params.name);
+    } else { //adding a conditional if the webpage is not found, 404
+    
+    response.json(description);
+});
+
 
 //using static middleware 
-app.use(express.static('public'));
+app.use(express.static('public'));   //to run: curl -i http://localhost:8080/blocks/Fixed
 
 app.get('/blocks', function(request, response) {  
     var blocks = ['Fixed', 'Movable', 'Rotating']; 
-    response.json(blocks);
+    if(request.query.limit >= 0) {
+        response.json(blocks.slice(0, request.query.limit));  //0, where the extraction begins and slice it all the way up, not including the number assigned to limit 
+        
+    } else {
+    response.json(blocks);  // use curl http://localhost:8080/blocks?limit=1
+    }
 });
 
 
